6ab14b5ca819cada2a40b071dd9fc57335495c37
Gerry Shona <gshona@ge-b5.kzn.21-school.ru>
2020-12-16 21:49:45 +0300

    auto commit

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -10,10 +10,14 @@ docker build -t nginx_image nginx
 docker build -t wordpress_image wordpress
 docker build -t phpmyadmin_image phpmyadmin
 docker build -t mysql_image mysql
+docker build -t grafana_image grafana
+docker build -t influxdb_image influxdb
 
 kubectl apply -f mysql/mysql.yaml
 kubectl apply -f nginx/nginx.yaml
 kubectl apply -f wordpress/wordpress.yaml
 kubectl apply -f phpmyadmin/phpmyadmin.yaml
+kubectl apply -f influxdb/influxdb.yaml
+kubectl apply -f grafana/grafana.yaml
 
 minikube dashboard
