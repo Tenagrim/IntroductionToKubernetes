2e96c15ed28f42a6bb50a6d0a24c401bca7c8f40
Gerry Shona <gshona@ge-f4.kzn.21-school.ru>
2020-12-20 15:54:01 +0300

    auto commit

--- ft_services/MySQL/start.sh
+++ ft_services/MySQL/start.sh
@@ -7,7 +7,18 @@ mariadb-install-db -u root
 
 mysqld -u root & sleep 5
 
-mysql -u root --execute="CREATE DATABASE gshona_db; CREATE USER 'admin'@'%' IDENTIFIED BY 'admin'; GRANT ALL PRIVILEGES ON gshona_db.* TO 'admin'@'%'; FLUSH PRIVILEGES;"
+if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='gshona_db'" 2>&1`" ]];
+then
+  echo "DATABASE ALREADY EXISTS"
+else
+	echo "DATABASE DOES NOT EXIST"
+	mysql -u root --execute="CREATE DATABASE gshona_db; CREATE USER 'admin'@'%' IDENTIFIED BY 'admin'; GRANT ALL PRIVILEGES ON gshona_db.* TO 'admin'@'%'; FLUSH PRIVILEGES;"
+	mysql -u root -p gshona_db --skip-password < /tmp/gshona_db.sql
+fi
+
+
+#mysql -u root --execute="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'DBName';"
+
 
 mysqladmin shutdown
 
